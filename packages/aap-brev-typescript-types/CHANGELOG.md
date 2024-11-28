1b171ad726021e644def6e46c79cd903a47803b0
Team Aap <team-aap@nav.no>
2024-11-28 07:18:33 +0000

    :label: genererte backendtyper

--- packages/aap-brev-typescript-types/CHANGELOG.md
+++ packages/aap-brev-typescript-types/CHANGELOG.md
@@ -1,5 +1,11 @@
 # @navikt/aap-brev-typescript-types
 
+## 0.0.23
+
+### Patch Changes
+
+- ny versjon av typescript typer
+
 ## 0.0.22
 
 ### Patch Changes
--- packages/aap-brev-typescript-types/schema.d.ts
+++ packages/aap-brev-typescript-types/schema.d.ts
@@ -468,12 +468,12 @@ export interface components {
             brevtype: "INNVILGELSE" | "AVSLAG";
             /**
              * Format: date-time
-             * @example 2024-11-27T11:53:20.077995011
+             * @example 2024-11-28T07:18:00.5200949
              */
             oppdatert: string;
             /**
              * Format: date-time
-             * @example 2024-11-27T11:53:20.077995011
+             * @example 2024-11-28T07:18:00.5200949
              */
             opprettet: string;
             /** Format: uuid */
@@ -511,7 +511,7 @@ export interface components {
         "no.nav.aap.brev.kontrakt.PdfBrev": {
             /**
              * Format: date
-             * @example 2024-11-27
+             * @example 2024-11-28
              */
             dato: string;
             mottaker: components["schemas"]["no.nav.aap.brev.kontrakt.PdfBrev.Mottaker"];
@@ -560,7 +560,7 @@ export interface components {
             navn: string;
             /**
              * Format: date-time
-             * @example 2024-11-27T11:53:20.077995011
+             * @example 2024-11-28T07:18:00.5200949
              */
             "planlagtKj\u00F8retidspunkt": string;
             /** @enum {string} */
