3e687b934d69c49949c6ec1370e0c73137670d95
Team Aap <team-aap@nav.no>
2025-01-22 10:06:17 +0000

    :label: genererte backendtyper

--- packages/aap-behandlingsflyt-typescript-types/schema.d.ts
+++ packages/aap-behandlingsflyt-typescript-types/schema.d.ts
@@ -2324,7 +2324,7 @@ export interface components {
             avIdent: string;
             /**
              * Format: date-time
-             * @example 2025-01-22T09:44:23.605965039
+             * @example 2025-01-22T10:05:30.92925502
              */
             tidspunkt: string;
         };
@@ -2577,12 +2577,12 @@ export interface components {
             mottaker: components["schemas"]["no.nav.aap.behandlingsflyt.behandling.brev.BrevGrunnlag.Brev.Mottaker"];
             /**
              * Format: date-time
-             * @example 2025-01-22T09:44:23.605965039
+             * @example 2025-01-22T10:05:30.92925502
              */
             oppdatert: string;
             /**
              * Format: date-time
-             * @example 2025-01-22T09:44:23.605965039
+             * @example 2025-01-22T10:05:30.92925502
              */
             opprettet: string;
             /** @enum {string} */
@@ -2780,7 +2780,7 @@ export interface components {
             fritekst: string;
             /**
              * Format: date-time
-             * @example 2025-01-22T09:44:23.605965039
+             * @example 2025-01-22T10:05:30.92925502
              */
             opprettet: string;
             personId: string;
@@ -2817,7 +2817,7 @@ export interface components {
             fraDato: string;
             /**
              * Format: date-time
-             * @example 2025-01-22T09:44:23.605965039
+             * @example 2025-01-22T10:05:30.92925502
              */
             vurderingsTidspunkt: string;
         };
@@ -2854,10 +2854,14 @@ export interface components {
             vurderinger: components["schemas"]["no.nav.aap.behandlingsflyt.faktagrunnlag.saksbehandler.barn.VurderingAvForeldreAnsvar"][];
         };
         "no.nav.aap.behandlingsflyt.faktagrunnlag.saksbehandler.beregning.BeregningYrkeskaderBel\u00F8pVurdering": {
+            /** Format: int64 */
+            id?: number | null;
             vurderinger: components["schemas"]["no.nav.aap.behandlingsflyt.faktagrunnlag.saksbehandler.beregning.YrkesskadeBel\u00F8pVurdering"][];
         };
         "no.nav.aap.behandlingsflyt.faktagrunnlag.saksbehandler.beregning.BeregningstidspunktVurdering": {
             begrunnelse: string;
+            /** Format: int64 */
+            id?: number | null;
             /**
              * Format: date
              * @example 2025-01-22
@@ -2997,7 +3001,7 @@ export interface components {
             harFritak: boolean;
             /**
              * Format: date-time
-             * @example 2025-01-22T09:44:23.605965039
+             * @example 2025-01-22T10:05:30.92925502
              */
             vurderingsTidspunkt: string;
         };
@@ -3138,7 +3142,7 @@ export interface components {
             avklaringsbehov: components["schemas"]["no.nav.aap.behandlingsflyt.flyt.AvklaringsbehovDTO"][];
             /**
              * Format: date-time
-             * @example 2025-01-22T09:44:23.605965039
+             * @example 2025-01-22T10:05:30.92925502
              */
             opprettet: string;
             /** Format: uuid */
@@ -3158,7 +3162,7 @@ export interface components {
             status: "OPPRETTET" | "AVSLUTTET" | "TOTRINNS_VURDERT" | "SENDT_TILBAKE_FRA_BESLUTTER" | "KVALITETSSIKRET" | "SENDT_TILBAKE_FRA_KVALITETSSIKRER" | "AVBRUTT";
             /**
              * Format: date-time
-             * @example 2025-01-22T09:44:23.605965039
+             * @example 2025-01-22T10:05:30.92925502
              */
             tidsstempel: string;
         };
@@ -3318,7 +3322,7 @@ export interface components {
             melding?: components["schemas"]["no.nav.aap.behandlingsflyt.kontrakt.hendelse.dokumenter.Melding"];
             /**
              * Format: date-time
-             * @example 2025-01-22T09:44:23.605965039
+             * @example 2025-01-22T10:05:30.92925502
              */
             mottattTidspunkt: string;
             referanse: components["schemas"]["no.nav.aap.behandlingsflyt.kontrakt.hendelse.InnsendingReferanse"];
@@ -3384,7 +3388,7 @@ export interface components {
             brevkode?: string | null;
             /**
              * Format: date-time
-             * @example 2025-01-22T09:44:23.605965039
+             * @example 2025-01-22T10:05:30.92925502
              */
             datoOpprettet: string;
             dokumentInfoId: string;
@@ -3397,7 +3401,7 @@ export interface components {
         "no.nav.aap.behandlingsflyt.sakogbehandling.sak.flate.BehandlinginfoDTO": {
             /**
              * Format: date-time
-             * @example 2025-01-22T09:44:23.605965039
+             * @example 2025-01-22T10:05:30.92925502
              */
             opprettet: string;
             /** Format: uuid */
@@ -3442,7 +3446,7 @@ export interface components {
             ident: string;
             /**
              * Format: date-time
-             * @example 2025-01-22T09:44:23.605965039
+             * @example 2025-01-22T10:05:30.92925502
              */
             opprettetTidspunkt: string;
             periode: components["schemas"]["no.nav.aap.komponenter.type.Periode"];
@@ -3453,7 +3457,7 @@ export interface components {
             ident: string;
             /**
              * Format: date-time
-             * @example 2025-01-22T09:44:23.605965039
+             * @example 2025-01-22T10:05:30.92925502
              */
             opprettetTidspunkt: string;
             periode: components["schemas"]["no.nav.aap.komponenter.type.Periode"];
@@ -3547,7 +3551,7 @@ export interface components {
             navn: string;
             /**
              * Format: date-time
-             * @example 2025-01-22T09:44:23.605965039
+             * @example 2025-01-22T10:05:30.92925502
              */
             "planlagtKj\u00F8retidspunkt": string;
             /** @enum {string} */
