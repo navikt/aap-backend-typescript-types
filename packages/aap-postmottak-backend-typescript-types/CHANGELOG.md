03e0a994f5d87006df3ad168ed520b58daae38bf
Team Aap <team-aap@nav.no>
2025-02-26 20:57:28 +0000

    :label: genererte backendtyper

--- packages/aap-postmottak-backend-typescript-types/CHANGELOG.md
+++ packages/aap-postmottak-backend-typescript-types/CHANGELOG.md
@@ -1,5 +1,11 @@
 # @navikt/aap-postmottak-backend-typescript-types
 
+## 0.0.146
+
+### Patch Changes
+
+- ny versjon av typescript typer
+
 ## 0.0.145
 
 ### Patch Changes
--- packages/aap-postmottak-backend-typescript-types/schema.d.ts
+++ packages/aap-postmottak-backend-typescript-types/schema.d.ts
@@ -977,7 +977,7 @@ export interface components {
             navn: string;
             /**
              * Format: date-time
-             * @example 2025-02-26T20:29:25.904295906
+             * @example 2025-02-26T20:56:52.495041981
              */
             "planlagtKj\u00F8retidspunkt": string;
             /** @enum {string} */
@@ -1070,7 +1070,7 @@ export interface components {
             avklaringsbehov: components["schemas"]["no.nav.aap.postmottak.api.flyt.AvklaringsbehovDTO"][];
             /**
              * Format: date-time
-             * @example 2025-02-26T20:29:25.904295906
+             * @example 2025-02-26T20:56:52.495041981
              */
             opprettet: string;
             referanse: components["schemas"]["no.nav.aap.postmottak.journalpostogbehandling.behandling.BehandlingsreferansePathParam"];
@@ -1088,7 +1088,7 @@ export interface components {
             status: "OPPRETTET" | "AVSLUTTET" | "SENDT_TILBAKE_FRA_BESLUTTER" | "SENDT_TILBAKE_FRA_KVALITETSSIKRER" | "AVBRUTT";
             /**
              * Format: date-time
-             * @example 2025-02-26T20:29:25.904295906
+             * @example 2025-02-26T20:56:52.495041981
              */
             tidsstempel: string;
         };
@@ -1198,7 +1198,7 @@ export interface components {
             id: string;
             /**
              * Format: date-time
-             * @example 2025-02-26T20:29:25.904295906
+             * @example 2025-02-26T20:56:52.495041981
              */
             opprettet: string;
             status: string;
