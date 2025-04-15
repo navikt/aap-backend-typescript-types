4f7c1817de993439284f65a840d22de09f1f4a74
Team Aap <team-aap@nav.no>
2025-04-15 12:40:34 +0000

    :label: genererte backendtyper

--- packages/aap-postmottak-backend-typescript-types/schema.d.ts
+++ packages/aap-postmottak-backend-typescript-types/schema.d.ts
@@ -1015,7 +1015,7 @@ export interface components {
             navn: string;
             /**
              * Format: date-time
-             * @example 2025-04-15T10:54:08.544363028
+             * @example 2025-04-15T12:39:50.028108142
              */
             "planlagtKj\u00F8retidspunkt": string;
             /** @enum {string} */
@@ -1109,7 +1109,7 @@ export interface components {
             avklaringsbehov: components["schemas"]["no.nav.aap.postmottak.api.flyt.AvklaringsbehovDTO"][];
             /**
              * Format: date-time
-             * @example 2025-04-15T10:54:08.544363028
+             * @example 2025-04-15T12:39:50.028108142
              */
             opprettet: string;
             referanse: components["schemas"]["no.nav.aap.postmottak.journalpostogbehandling.behandling.BehandlingsreferansePathParam"];
@@ -1127,7 +1127,7 @@ export interface components {
             status: "OPPRETTET" | "AVSLUTTET" | "SENDT_TILBAKE_FRA_BESLUTTER" | "SENDT_TILBAKE_FRA_KVALITETSSIKRER" | "AVBRUTT";
             /**
              * Format: date-time
-             * @example 2025-04-15T10:54:08.544363028
+             * @example 2025-04-15T12:39:50.028108142
              */
             tidsstempel: string;
         };
@@ -1237,7 +1237,7 @@ export interface components {
             id: string;
             /**
              * Format: date-time
-             * @example 2025-04-15T10:54:08.544363028
+             * @example 2025-04-15T12:39:50.028108142
              */
             opprettet: string;
             status: string;
