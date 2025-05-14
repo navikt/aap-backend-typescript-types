33e47a8945a061fa1cb3bf1151f7cd5ed92ba143
Team Aap <team-aap@nav.no>
2025-05-14 08:44:23 +0000

    :label: genererte backendtyper

--- packages/aap-brev-typescript-types/schema.d.ts
+++ packages/aap-brev-typescript-types/schema.d.ts
@@ -713,12 +713,12 @@ export interface components {
             brevtype: "INNVILGELSE" | "AVSLAG" | "VARSEL_OM_BESTILLING" | "FORHÅNDSVARSEL_BRUDD_AKTIVITETSPLIKT" | "FORVALTNINGSMELDING" | "VEDTAK_ENDRING";
             /**
              * Format: date-time
-             * @example 2025-05-14T05:27:35.44876181
+             * @example 2025-05-14T08:43:49.384335605
              */
             oppdatert: string;
             /**
              * Format: date-time
-             * @example 2025-05-14T05:27:35.44876181
+             * @example 2025-05-14T08:43:49.384335605
              */
             opprettet: string;
             /** Format: uuid */
@@ -815,12 +815,12 @@ export interface components {
             navn: string;
             /**
              * Format: date-time
-             * @example 2025-05-14T05:27:35.44876181
+             * @example 2025-05-14T08:43:49.384335605
              */
             opprettetTidspunkt?: string | null;
             /**
              * Format: date-time
-             * @example 2025-05-14T05:27:35.44876181
+             * @example 2025-05-14T08:43:49.384335605
              */
             "planlagtKj\u00F8retidspunkt": string;
             /** @enum {string} */
