9b246201a6190c991cfba4fbc96ab66e818db26a
Team Aap <team-aap@nav.no>
2025-05-28 06:27:35 +0000

    :label: genererte backendtyper

--- packages/aap-postmottak-backend-typescript-types/schema.d.ts
+++ packages/aap-postmottak-backend-typescript-types/schema.d.ts
@@ -992,12 +992,12 @@ export interface components {
         "no.nav.aap.komponenter.type.Periode": {
             /**
              * Format: date
-             * @example 2025-05-27
+             * @example 2025-05-28
              */
             fom: string;
             /**
              * Format: date
-             * @example 2025-05-27
+             * @example 2025-05-28
              */
             tom: string;
         };
@@ -1015,12 +1015,12 @@ export interface components {
             navn: string;
             /**
              * Format: date-time
-             * @example 2025-05-27T06:24:43.719388671
+             * @example 2025-05-28T06:26:55.018749383
              */
             opprettetTidspunkt?: string | null;
             /**
              * Format: date-time
-             * @example 2025-05-27T06:24:43.719388671
+             * @example 2025-05-28T06:26:55.018749383
              */
             "planlagtKj\u00F8retidspunkt": string;
             /** @enum {string} */
@@ -1042,7 +1042,7 @@ export interface components {
             journalpostId: number;
             /**
              * Format: date
-             * @example 2025-05-27
+             * @example 2025-05-28
              */
             registrertDato?: string | null;
             "s\u00F8ker"?: components["schemas"]["no.nav.aap.postmottak.api.faktagrunnlag.dokument.DokumentIdent"];
@@ -1076,7 +1076,7 @@ export interface components {
             strukturertDokumentJson?: string | null;
             /**
              * Format: date
-             * @example 2025-05-27
+             * @example 2025-05-28
              */
             "s\u00F8knadsdato"?: string | null;
         };
@@ -1114,7 +1114,7 @@ export interface components {
             avklaringsbehov: components["schemas"]["no.nav.aap.postmottak.api.flyt.AvklaringsbehovDTO"][];
             /**
              * Format: date-time
-             * @example 2025-05-27T06:24:43.719388671
+             * @example 2025-05-28T06:26:55.018749383
              */
             opprettet: string;
             referanse: components["schemas"]["no.nav.aap.postmottak.journalpostogbehandling.behandling.BehandlingsreferansePathParam"];
@@ -1132,7 +1132,7 @@ export interface components {
             status: "OPPRETTET" | "AVSLUTTET" | "SENDT_TILBAKE_FRA_BESLUTTER" | "SENDT_TILBAKE_FRA_KVALITETSSIKRER" | "AVBRUTT";
             /**
              * Format: date-time
-             * @example 2025-05-27T06:24:43.719388671
+             * @example 2025-05-28T06:26:55.018749383
              */
             tidsstempel: string;
         };
@@ -1163,7 +1163,7 @@ export interface components {
             behandlingVersjon: number;
             /**
              * Format: date
-             * @example 2025-05-27
+             * @example 2025-05-28
              */
             frist?: string | null;
             /** @enum {string} */
@@ -1173,7 +1173,7 @@ export interface components {
             begrunnelse: string;
             /**
              * Format: date
-             * @example 2025-05-27
+             * @example 2025-05-28
              */
             frist: string;
             /** @enum {string} */
@@ -1208,7 +1208,7 @@ export interface components {
             strukturertDokument?: string | null;
             /**
              * Format: date
-             * @example 2025-05-27
+             * @example 2025-05-28
              */
             "s\u00F8knadsdato"?: string | null;
         };
@@ -1242,7 +1242,7 @@ export interface components {
             id: string;
             /**
              * Format: date-time
-             * @example 2025-05-27T06:24:43.719388671
+             * @example 2025-05-28T06:26:55.018749383
              */
             opprettet: string;
             status: string;
