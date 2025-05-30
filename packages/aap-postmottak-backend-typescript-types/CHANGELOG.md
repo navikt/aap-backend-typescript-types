f61559f3f29b4bebb21126839a428ec82f811aa5
Team Aap <team-aap@nav.no>
2025-05-30 07:48:38 +0000

    :label: genererte backendtyper

--- packages/aap-postmottak-backend-typescript-types/CHANGELOG.md
+++ packages/aap-postmottak-backend-typescript-types/CHANGELOG.md
@@ -1,5 +1,11 @@
 # @navikt/aap-postmottak-backend-typescript-types
 
+## 0.0.265
+
+### Patch Changes
+
+- ny versjon av typescript typer
+
 ## 0.0.264
 
 ### Patch Changes
--- packages/aap-postmottak-backend-typescript-types/schema.d.ts
+++ packages/aap-postmottak-backend-typescript-types/schema.d.ts
@@ -1015,12 +1015,12 @@ export interface components {
             navn: string;
             /**
              * Format: date-time
-             * @example 2025-05-30T07:23:16.239902622
+             * @example 2025-05-30T07:47:55.124649848
              */
             opprettetTidspunkt?: string | null;
             /**
              * Format: date-time
-             * @example 2025-05-30T07:23:16.239902622
+             * @example 2025-05-30T07:47:55.124649848
              */
             "planlagtKj\u00F8retidspunkt": string;
             /** @enum {string} */
@@ -1114,7 +1114,7 @@ export interface components {
             avklaringsbehov: components["schemas"]["no.nav.aap.postmottak.api.flyt.AvklaringsbehovDTO"][];
             /**
              * Format: date-time
-             * @example 2025-05-30T07:23:16.239902622
+             * @example 2025-05-30T07:47:55.124649848
              */
             opprettet: string;
             referanse: components["schemas"]["no.nav.aap.postmottak.journalpostogbehandling.behandling.BehandlingsreferansePathParam"];
@@ -1132,7 +1132,7 @@ export interface components {
             status: "OPPRETTET" | "AVSLUTTET" | "SENDT_TILBAKE_FRA_BESLUTTER" | "SENDT_TILBAKE_FRA_KVALITETSSIKRER" | "AVBRUTT";
             /**
              * Format: date-time
-             * @example 2025-05-30T07:23:16.239902622
+             * @example 2025-05-30T07:47:55.124649848
              */
             tidsstempel: string;
         };
@@ -1242,7 +1242,7 @@ export interface components {
             id: string;
             /**
              * Format: date-time
-             * @example 2025-05-30T07:23:16.239902622
+             * @example 2025-05-30T07:47:55.124649848
              */
             opprettet: string;
             status: string;
