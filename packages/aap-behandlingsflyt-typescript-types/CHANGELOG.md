31d53a8d84548d34330005bedaa0d29a9ce28d3c
Team Aap <team-aap@nav.no>
2025-11-06 07:27:14 +0000

    :label: genererte backendtyper for behandlingsflyt

--- packages/aap-behandlingsflyt-typescript-types/CHANGELOG.md
+++ packages/aap-behandlingsflyt-typescript-types/CHANGELOG.md
@@ -1,5 +1,11 @@
 # @navikt/aap-behandlingsflyt-typescript-types
 
+## 0.0.2637
+
+### Patch Changes
+
+- ny versjon av typescript typer
+
 ## 0.0.2636
 
 ### Patch Changes
--- packages/aap-behandlingsflyt-typescript-types/schema.d.ts
+++ packages/aap-behandlingsflyt-typescript-types/schema.d.ts
@@ -239,41 +239,6 @@ export interface paths {
         patch?: never;
         trace?: never;
     };
-    "/api/sak/alle": {
-        parameters: {
-            query?: never;
-            header?: never;
-            path?: never;
-            cookie?: never;
-        };
-        get: {
-            parameters: {
-                query?: never;
-                header?: never;
-                path?: never;
-                cookie?: never;
-            };
-            requestBody?: never;
-            responses: {
-                /** @description OK */
-                200: {
-                    headers: {
-                        [name: string]: unknown;
-                    };
-                    content: {
-                        "application/json": components["schemas"]["no.nav.aap.behandlingsflyt.sakogbehandling.sak.flate.SaksinfoDTO"][];
-                    };
-                };
-            };
-        };
-        put?: never;
-        post?: never;
-        delete?: never;
-        options?: never;
-        head?: never;
-        patch?: never;
-        trace?: never;
-    };
     "/api/sak/siste/{antall}": {
         parameters: {
             query?: never;
