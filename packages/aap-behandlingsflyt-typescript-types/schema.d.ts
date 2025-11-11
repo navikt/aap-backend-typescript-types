2cb9d5e5bc6ef5bc2d1cb045e1ab1b013311f585
Team Aap <team-aap@nav.no>
2025-11-11 14:46:49 +0000

    :label: genererte backendtyper for behandlingsflyt

--- packages/aap-behandlingsflyt-typescript-types/schema.d.ts
+++ packages/aap-behandlingsflyt-typescript-types/schema.d.ts
@@ -5107,7 +5107,7 @@ export interface components {
         "no.nav.aap.behandlingsflyt.behandling.foresl\u00E5vedtak.Avslags\u00E5rsakDto": {
             /** @enum {string|null} */
             underveisavslag?: "IKKE_GRUNNLEGGENDE_RETT" | "MELDEPLIKT_FRIST_IKKE_PASSERT" | "IKKE_OVERHOLDT_MELDEPLIKT_SANKSJON" | "ARBEIDER_MER_ENN_GRENSEVERDI" | "SONER_STRAFF" | "VARIGHETSKVOTE_BRUKT_OPP" | "BRUDD_PÅ_AKTIVITETSPLIKT_11_7_STANS" | "BRUDD_PÅ_AKTIVITETSPLIKT_11_7_OPPHØR" | "BRUDD_PÅ_OPPHOLDSKRAV_11_3_STANS" | "BRUDD_PÅ_OPPHOLDSKRAV_11_3_OPPHØR" | null;
-            "vilk\u00E5rsavslag": ("BRUKER_UNDER_18" | "BRUKER_OVER_67" | "MANGLENDE_DOKUMENTASJON" | "IKKE_SYKDOM_AV_VISS_VARIGHET" | "IKKE_SYKDOM_SKADE_LYTE_VESENTLIGDEL" | "IKKE_NOK_REDUSERT_ARBEIDSEVNE" | "IKKE_BEHOV_FOR_OPPFOLGING" | "IKKE_MEDLEM_FORUTGÅENDE" | "IKKE_MEDLEM" | "IKKE_OPPFYLT_OPPHOLDSKRAV_EØS" | "NORGE_IKKE_KOMPETENT_STAT" | "ANNEN_FULL_YTELSE" | "IKKE_RETT_PA_AAP_UNDER_BEHANDLING_AV_UFORE" | "VARIGHET_OVERSKREDET_OVERGANG_UFORE" | "VARIGHET_OVERSKREDET_ARBEIDSSØKER" | "IKKE_RETT_PA_AAP_I_PERIODE_SOM_ARBEIDSSOKER")[];
+            "vilk\u00E5rsavslag": string[];
         };
         "no.nav.aap.behandlingsflyt.behandling.foresl\u00E5vedtak.Foresl\u00E5VedtakDto": {
             "avslags\u00E5rsak": components["schemas"]["no.nav.aap.behandlingsflyt.behandling.foresl\u00E5vedtak.Avslags\u00E5rsakDto"];
