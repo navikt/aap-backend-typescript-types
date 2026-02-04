0ed17ca98ddd851894e76c2856ec162be509f305
Team Aap <team-aap@nav.no>
2026-02-04 15:10:09 +0000

    :label: genererte backendtyper for behandlingsflyt

--- packages/aap-behandlingsflyt-typescript-types/schema.d.ts
+++ packages/aap-behandlingsflyt-typescript-types/schema.d.ts
@@ -6128,6 +6128,7 @@ export interface components {
             gjeldendeVurdering?: components["schemas"]["no.nav.aap.behandlingsflyt.behandling.student.sykestipend.SykestipendvurderingResponse"];
             "harTilgangTil\u00C5Saksbehandle": boolean;
             historiskeVurderinger: components["schemas"]["no.nav.aap.behandlingsflyt.behandling.student.sykestipend.SykestipendvurderingResponse"][];
+            "sykeStipendSvarFraS\u00F8knad"?: boolean | null;
         };
         "no.nav.aap.behandlingsflyt.behandling.student.sykestipend.SykestipendvurderingResponse": {
             begrunnelse: string;
