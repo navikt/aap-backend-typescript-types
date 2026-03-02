fd494002b4bf35e7c4263487ce423cb2f27d62e7
Team Aap <team-aap@nav.no>
2026-03-02 16:49:23 +0000

    :label: genererte backendtyper for oppgave

--- packages/aap-oppgave-typescript-types/dist/index.d.ts
+++ packages/aap-oppgave-typescript-types/dist/index.d.ts
@@ -951,6 +951,41 @@ interface paths {
         patch?: never;
         trace?: never;
     };
+    "/drift/api/jobb/feilende/antall": {
+        parameters: {
+            query?: never;
+            header?: never;
+            path?: never;
+            cookie?: never;
+        };
+        get: {
+            parameters: {
+                query?: never;
+                header?: never;
+                path?: never;
+                cookie?: never;
+            };
+            requestBody?: never;
+            responses: {
+                /** @description OK */
+                200: {
+                    headers: {
+                        [name: string]: unknown;
+                    };
+                    content: {
+                        "application/json": number;
+                    };
+                };
+            };
+        };
+        put?: never;
+        post?: never;
+        delete?: never;
+        options?: never;
+        head?: never;
+        patch?: never;
+        trace?: never;
+    };
     "/drift/api/jobb/planlagte-jobber": {
         parameters: {
             query?: never;
