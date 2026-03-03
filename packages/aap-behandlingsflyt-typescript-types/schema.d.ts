a54a8d7f008946a61b545225d9d9d8e99abfbd24
Team Aap <team-aap@nav.no>
2026-03-03 09:16:02 +0000

    :label: genererte backendtyper for behandlingsflyt

--- packages/aap-behandlingsflyt-typescript-types/schema.d.ts
+++ packages/aap-behandlingsflyt-typescript-types/schema.d.ts
@@ -7942,7 +7942,7 @@ export interface components {
             avslag12_5: boolean;
             /** @enum {string} */
             kilde: "PENSJON";
-            personid: string;
+            personId: string;
             /** @enum {string} */
             resultat: "AVSL" | "ENDR" | "INNV" | "OPPH";
             /**
