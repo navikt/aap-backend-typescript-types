7505c1d0a5bb66115e5a95079b0f519b71b5abd3
Team Aap <team-aap@nav.no>
2026-03-16 07:10:13 +0000

    :label: genererte backendtyper for behandlingsflyt

--- packages/aap-behandlingsflyt-typescript-types/schema.d.ts
+++ packages/aap-behandlingsflyt-typescript-types/schema.d.ts
@@ -4338,6 +4338,42 @@ export interface paths {
         patch?: never;
         trace?: never;
     };
+    "/api/drift/behandling/{referanse}/utvid-rettighetsperiode-og-kjor-fra-start": {
+        parameters: {
+            query?: never;
+            header?: never;
+            path?: never;
+            cookie?: never;
+        };
+        get?: never;
+        put?: never;
+        post: {
+            parameters: {
+                query?: never;
+                header?: never;
+                path: {
+                    /** @description referanse */
+                    referanse: string;
+                };
+                cookie?: never;
+            };
+            requestBody?: never;
+            responses: {
+                /** @description OK */
+                200: {
+                    headers: {
+                        [name: string]: unknown;
+                    };
+                    content?: never;
+                };
+            };
+        };
+        delete?: never;
+        options?: never;
+        head?: never;
+        patch?: never;
+        trace?: never;
+    };
     "/api/drift/brev/{brevbestillingReferanse}/avbryt": {
         parameters: {
             query?: never;
