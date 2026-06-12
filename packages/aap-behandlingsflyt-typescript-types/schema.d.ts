1ee31dd463a097366707b2cda5207be91db4822f
Team Aap <team-aap@nav.no>
2026-06-12 08:24:46 +0000

    :label: genererte backendtyper for behandlingsflyt

--- packages/aap-behandlingsflyt-typescript-types/schema.d.ts
+++ packages/aap-behandlingsflyt-typescript-types/schema.d.ts
@@ -7013,6 +7013,11 @@ export interface components {
              * @example 2025-04-01
              */
             meldeDato: string;
+            /**
+             * Format: date
+             * @example 2025-04-01
+             */
+            mottattTidspunkt?: string | null;
             oppdatertAv?: string | null;
             oppdatertAvSaksbehandler: boolean;
             /**
@@ -7033,6 +7038,7 @@ export interface components {
             meldeDato?: string | null;
             meldekort?: components["schemas"]["no.nav.aap.behandlingsflyt.behandling.meldekort.MeldekortDto"];
             meldeperiode: components["schemas"]["no.nav.aap.komponenter.type.Periode"];
+            meldepliktStatus: ("FREMTIDIG_IKKE_OPPFYLT" | "FREMTIDIG_OPPFYLT" | "FRITAK" | "FØRSTE_MELDEPERIODE_MED_RETT" | "FØR_VEDTAK" | "IKKE_MELDT_SEG" | "MELDT_SEG" | "RIMELIG_GRUNN" | "UTEN_RETT")[];
             periode?: components["schemas"]["no.nav.aap.komponenter.type.Periode"];
             tidligereMeldekort: components["schemas"]["no.nav.aap.behandlingsflyt.behandling.meldekort.MeldekortDto"][];
         };
