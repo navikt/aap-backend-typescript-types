4e271cfc7a7e15555561e1fb6520cb75103c59f1
Team Aap <team-aap@nav.no>
2026-07-17 15:45:49 +0000

    :label: genererte backendtyper for behandlingsflyt

--- packages/aap-behandlingsflyt-typescript-types/schema.d.ts
+++ packages/aap-behandlingsflyt-typescript-types/schema.d.ts
@@ -5709,7 +5709,7 @@ export interface components {
         "no.nav.aap.behandlingsflyt.behandling.avklaringsbehov.flate.Historikk": {
             /** @enum {string} */
             aksjon: "FATTET_VEDTAK" | "KVALITETSSIKRET" | "RETURNERT_FRA_BESLUTTER" | "RETURNERT_FRA_KVALITETSSIKRER" | "SENDT_TIL_BESLUTTER" | "SENDT_TIL_KVALITETSSIKRER";
-            avIdent: string;
+            avIdent: components["schemas"]["no.nav.aap.komponenter.verdityper.Bruker"];
             /**
              * Format: date-time
              * @example 2025-04-01T12:30:00
@@ -8076,7 +8076,7 @@ export interface components {
         };
         "no.nav.aap.behandlingsflyt.drift.ForenkletAvklaringsbehov": {
             definisjon: components["schemas"]["no.nav.aap.behandlingsflyt.kontrakt.avklaringsbehov.Definisjon"];
-            endretAv: string;
+            endretAv: components["schemas"]["no.nav.aap.komponenter.verdityper.Bruker"];
             /**
              * Format: date
              * @example 2025-04-01
@@ -9023,7 +9023,7 @@ export interface components {
         };
         "no.nav.aap.behandlingsflyt.flyt.EndringDTO": {
             begrunnelse?: string | null;
-            endretAv: string;
+            endretAv: components["schemas"]["no.nav.aap.komponenter.verdityper.Bruker"];
             /** @enum {string} */
             status: "AVBRUTT" | "AVSLUTTET" | "KVALITETSSIKRET" | "OPPRETTET" | "SENDT_TILBAKE_FRA_BESLUTTER" | "SENDT_TILBAKE_FRA_KVALITETSSIKRER" | "TOTRINNS_VURDERT";
             /**
@@ -9610,7 +9610,7 @@ export interface components {
              * @example 2025-04-01T12:30:00
              */
             opprettet: string;
-            opprettetAv?: string | null;
+            opprettetAv?: components["schemas"]["no.nav.aap.komponenter.verdityper.Bruker"];
             vurderingsbehov: components["schemas"]["no.nav.aap.behandlingsflyt.sakogbehandling.behandling.VurderingsbehovMedPeriode"][];
             /** @enum {string} */
             "\u00E5rsak": "AKTIVITETSMELDING" | "AKTIVITETSPLIKT" | "AKTIVITETSPLIKT_11_9" | "ANNET_RELEVANT_DOKUMENT" | "BARNETILLEGG_SATSENDRING" | "ENDRING_I_REGISTERDATA" | "FAGSYSTEMINFO_BEHOV_HENDELSE" | "FASTSATT_PERIODE_PASSERT" | "FRITAK_MELDEPLIKT" | "G_REGULERING" | "HELSEOPPLYSNINGER" | "KLAGE" | "MANUELL_OPPRETTELSE" | "MELDEKORT" | "MIGRER_RETTIGHETSPERIODE" | "OMGJØRING_ETTER_KLAGE" | "OMGJØRING_ETTER_SVAR_FRA_KLAGEINSTANS" | "OPPFØLGINGSOPPGAVE" | "OPPFØLGINGSOPPGAVE_SAMORDNING_GRADERING" | "SVAR_FRA_KLAGEINSTANS" | "SØKNAD" | "TILBAKEKREVING_HENDELSE" | "UFØRE_VEDTAK_HENDELSE" | "UTVID_VEDTAKSLENGDE";
