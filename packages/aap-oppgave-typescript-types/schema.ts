0fce836b38d0fa0bac8bdca7dcb50ed062ed8b89
Team Aap <team-aap@nav.no>
2026-08-04 06:26:04 +0000

    :label: genererte backendtyper for oppgave

--- packages/aap-oppgave-typescript-types/schema.ts
+++ packages/aap-oppgave-typescript-types/schema.ts
@@ -846,44 +846,6 @@ export interface paths {
         patch?: never;
         trace?: never;
     };
-    "/filter/v2": {
-        parameters: {
-            query?: never;
-            header?: never;
-            path?: never;
-            cookie?: never;
-        };
-        get: {
-            parameters: {
-                query?: {
-                    /** @description Enhetsfilter */
-                    enheter?: string[];
-                };
-                header?: never;
-                path?: never;
-                cookie?: never;
-            };
-            requestBody?: never;
-            responses: {
-                /** @description OK */
-                200: {
-                    headers: {
-                        [name: string]: unknown;
-                    };
-                    content: {
-                        "application/json": components["schemas"]["no.nav.aap.oppgave.filter.FilterResponse"][];
-                    };
-                };
-            };
-        };
-        put?: never;
-        post?: never;
-        delete?: never;
-        options?: never;
-        head?: never;
-        patch?: never;
-        trace?: never;
-    };
     "/enhet/status/person": {
         parameters: {
             query?: never;
